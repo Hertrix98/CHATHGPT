b8e9aa1269f0f042aa693596efe826fe2609733a
Hertrix98 <133254477+Hertrix98@users.noreply.github.com>
2025-12-13 21:51:39 -0600

    Add shared layout with navigation and footer (#2)
    * Add shared layout with navigation and footer
    
    * Apply shared layout to welcome page

---
+++ resources/views/partials/menu.blade.php
@@ -0,0 +1,9 @@
+<div class="container nav" aria-label="Main navigation">
+    <a class="brand" href="/">{{ config('app.name') }}</a>
+    <ul>
+        <li><a href="#funcionalidad">Funcionalidad</a></li>
+        <li><a href="#flujo">Flujo de sincronización</a></li>
+        <li><a href="#seguridad">Seguridad</a></li>
+        <li><a href="#contacto">Contacto</a></li>
+    </ul>
+</div>
